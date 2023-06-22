6bfc08471c658e658cde73ae751e25622e23ec40
tsmsp <tsm_sp@hotmail.com>
2023-06-21 21:33:42 -0700

    clean code

--- src/infra/cms/CMSSectionRender.js
+++ src/infra/cms/CMSSectionRender.js
@@ -5,10 +5,7 @@ export function CMSSectionRender({ pageName }) {
   const sections = getCMSContent(`${pageName}.pageContent[0].section`);
 
   return sections.map((sectionProps) => {
-    console.log("wiii>>", sectionProps);
     const Component = cmsSections[sectionProps.componentName];
-    const commonSeoBlockRecord = Component && Component.CommonSeoBlockRecord;
-
     const isVisible =
       sectionProps.visible === true || sectionProps.visible === undefined;
 
