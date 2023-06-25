b1d799aad4c2818c9c12c79d07bf0e8024520a2c
tsmsp <tsm_sp@hotmail.com>
2023-06-24 19:36:56 -0700

    add try

--- src/screens/FAQAllQuestionsScreen/index.js
+++ src/screens/FAQAllQuestionsScreen/index.js
@@ -1,58 +1,65 @@
-import Head from "next/head";
-import { Footer } from "../../components/commons/Footer";
-import { Menu } from "../../components/commons/Menu";
+import { CMSProvider } from "../../infra/cms/CMSProvider";
 import { pageHOC } from "../../components/wrappers/pageHOC";
 import { CMSSectionRender } from "../../infra/cms/CMSSectionRender";
-import { cmsService } from "../../infra/cms/cmsService";
-import { Box, Text, Link, Image, theme } from "../../theme/components";
+import { cmsService } from "../../infra/cms/CMSService";
 
 export async function getStaticProps({ preview }) {
-  const { data: cmsContent } = await cmsService({
-    query: `
-    query {
-      pageFaq {
-        pageContent {
-          section {
-            componentName: __typename
-            ... on CommonSeoBlockRecord {
-              id
-              title
-            }
-            ... on CommonMenuRecord {
-              id
-            }
-            ... on CommonFooterRecord {
-              id
-              visible
-            }
-            ... on PagefaqDisplayquestionSectionRecord {
-              id
-              categories {
+  try {
+    const { data: cmsContent } = await cmsService({
+      query: `
+      query {
+        pageFaq {
+          pageContent {
+            section {
+              componentName: __typename
+              ... on CommonSeoBlockRecord {
                 id
                 title
-                questions {
+              }
+              ... on CommonMenuRecord {
+                id
+              }
+              ... on CommonFooterRecord {
+                id                
+              }
+              ... on PagefaqDisplayquestionSectionRecord {
+                id
+                categories {
                   id
                   title
+                  questions {
+                    id
+                    title
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-    `,
-    preview,
-  });
+      `,
+      preview,
+    });
 
-  return {
-    props: {
-      cmsContent,
-    },
-  };
+    return {
+      props: {
+        cmsContent,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching CMS data:", error);
+    return {
+      props: {
+        cmsContent: {},
+      },
+    };
+  }
 }
 
-function FAQAllQuestionsScreen() {
-  return <CMSSectionRender pageName="pageFaq" />;
-}
-
-export default pageHOC(FAQAllQuestionsScreen);
+export default pageHOC(function FAQAllQuestionsScreen({ cmsContent }) {
+  return (
+    <CMSProvider cmsContent={cmsContent}>
+      <CMSSectionRender pageName="pageFaq" />
+    </CMSProvider>
+  );
+});
