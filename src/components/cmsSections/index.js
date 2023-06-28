2ec9dfd4baf84474b71b29b9ac5bb02fffbb723a
tsmsp <tsm_sp@hotmail.com>
2023-06-27 20:58:02 -0700

    renamed variable

--- src/components/cmsSections/index.js
+++ src/components/cmsSections/index.js
@@ -4,13 +4,12 @@ import { PageFAQDisplayQuestionsSection } from "./PageFAQDisplayQuestionsSection
 import { PageHomeHeroSection } from "./PageHomeHeroSection";
 import { SEOBlock } from "./SeoBlock";
 
-export const CMSSections = {
+export const cmsSections = {
   PagefaqDisplayquestionSectionRecord: (props) => (
     <PageFAQDisplayQuestionsSection {...props} />
   ),
-  // CommonSeoBlockRecord: (props) => <SEOBlock {...props} />,
-  CommonSeoBlockRecord: SEOBlock,
-  CommomMenuRecord: (props) => <Menu {...props} />,
+  CommonSeoBlockRecord: (props) => <SEOBlock {...props} />,
+  CommonMenuRecord: (props) => <Menu {...props} />,
   PagehomeHerosectionRecord: (props) => <PageHomeHeroSection {...props} />,
-  CommomFooterRecord: (props) => <Footer {...props} />,
+  CommonFooterRecord: (props) => <Footer {...props} />,
 };
