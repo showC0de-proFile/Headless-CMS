800477e610244ed389e2c632366784174e6f216b
tsmsp <tsm_sp@hotmail.com>
2023-06-27 20:32:41 -0700

    added endpoint -> questions

--- src/screens/FAQAllQuestionsScreen/index.js
+++ src/screens/FAQAllQuestionsScreen/index.js
@@ -1,65 +1,54 @@
-import { CMSProvider } from "../../infra/cms/CMSProvider";
 import { pageHOC } from "../../components/wrappers/pageHOC";
 import { CMSSectionRender } from "../../infra/cms/CMSSectionRender";
-import { cmsService } from "../../infra/cms/CMSService";
+import { cmsService } from "../../infra/cms/cmsService";
 
 export async function getStaticProps({ preview }) {
-  try {
-    const { data: cmsContent } = await cmsService({
-      query: `
-      query {
-        pageFaq {
-          pageContent {
-            section {
-              componentName: __typename
-              ... on CommonSeoBlockRecord {
+  const { data: cmsContent } = await cmsService({
+    query: `
+    query {
+      pageFaq {
+        pageContent {
+          section {
+            componentName: __typename
+            ... on CommonSeoBlockRecord {
+              id
+              title
+            }
+            ... on CommonMenuRecord {
+              id
+            }
+            ... on CommonFooterRecord {
+              id
+              visible
+            }
+            ... on PagefaqDisplayquestionSectionRecord {
+              id
+              categories {
                 id
                 title
-              }
-              ... on CommonMenuRecord {
-                id
-              }
-              ... on CommonFooterRecord {
-                id                
-              }
-              ... on PagefaqDisplayquestionSectionRecord {
-                id
-                categories {
+                questions {
                   id
                   title
-                  questions {
-                    id
-                    title
-                  }
                 }
               }
             }
           }
         }
       }
-      `,
-      preview,
-    });
+    }
+    `,
+    preview,
+  });
 
-    return {
-      props: {
-        cmsContent,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching CMS data:", error);
-    return {
-      props: {
-        cmsContent: {},
-      },
-    };
-  }
+  return {
+    props: {
+      cmsContent,
+    },
+  };
 }
 
-export default pageHOC(function FAQAllQuestionsScreen({ cmsContent }) {
-  return (
-    <CMSProvider cmsContent={cmsContent}>
-      <CMSSectionRender pageName="pageFaq" />
-    </CMSProvider>
-  );
-});
+function FAQAllQuestionsScreen() {
+  return <CMSSectionRender pageName="pageFaq" />;
+}
+
+export default pageHOC(FAQAllQuestionsScreen);
