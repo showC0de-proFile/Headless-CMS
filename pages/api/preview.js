7f1810edbbf2e51181b7f8c226ef5903d8caf351
tsmsp <tsm_sp@hotmail.com>
2023-07-04 15:06:40 -0700

    valid password

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,19 +1,19 @@
 export default async function handler(req, res) {
   const previousPage = req.headers.referer;
 
-  // SE TIVER LIGADO O PREVIEW: DESLIGA
-  if(req.preview) {
+  // IF YOU HAVE VISUALIZATION ON: TURN OFF
+  if (req.preview) {
     res.clearPreviewData();
     res.writeHead(307, { Location: previousPage });
     return res.end();
   }
 
-  const password = 'SENHASEGURA';
-  if(req.query.password !== password) {
-    return res.status(401).json({ message: 'Invalid Password' });
+  const password = "PASSWORD";
+  if (req.query.password !== password) {
+    return res.status(401).json({ message: "Invalid Password" });
   }
-  
-  // SE TIVER DESLIGADO O PREVIEW: LIGA
+
+  // IF YOU HAVE TURNED OFF PREVIEW: TURN ON
   res.setPreviewData({});
   res.writeHead(307, { Location: previousPage });
   res.end();
